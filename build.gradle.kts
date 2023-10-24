e26a76f2aeaa557acfdbe60ea6fc7ae7e53b9999
Ed Zhao <edward.zhao@affirm.com>
2023-10-24 15:47:54 -0700

    Minimal production repo

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.springframework.boot:spring-boot-gradle-plugin:2.5.5")
+    }
+    configurations.classpath {
+        resolutionStrategy.activateDependencyLocking()
+    }
+}
+
+plugins {
+    kotlin("jvm") version "1.8.22"
+}
+
+apply(plugin = "org.springframework.boot")
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.5")
+    implementation("org.springframework.boot:spring-boot-starter-web:2.5.5")
+}
+
+configurations.all {
+    resolutionStrategy.activateDependencyLocking()
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
